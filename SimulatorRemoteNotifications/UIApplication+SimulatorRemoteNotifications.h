00306ae679f9497dcfc97c64a286dcb793a74316
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-02-22 19:36:36 -0800

    Added documentation

--- SimulatorRemoteNotifications/UIApplication+SimulatorRemoteNotifications.h
+++ SimulatorRemoteNotifications/UIApplication+SimulatorRemoteNotifications.h
@@ -8,10 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
+
+/** SimulatorRemoteNotifications is a category that adds remote notifications by listening on a UDP port. This makes it effectively possible to receive mock remote notifications in the iOS simulator
+ */
 @interface UIApplication (SimulatorRemoteNotifications)
 
+/** @name Properties */
+
+/** Remote notification port
+ */
 @property (nonatomic, assign) NSInteger remoteNotificationsPort;
 
+
+/** @name Listening to notifications */
+
+/** Start listening for remote notifications
+ * @discussion The application will start listening for remote notifications via UDP. You can use the ACSimulatorRemoteNotificationsService to send notification to the application, calling the app delegate's application:didReceiveRemoteNotification:fetchCompletionHandler: (iOS7 and later) or application:didReceiveRemoteNotification: method.
+ */
 - (void)listenForRemoteNotifications;
 
 @end
--- SimulatorRemoteNotificationsLib/ACSimulatorRemoteNotificationsService.h
+++ SimulatorRemoteNotificationsLib/ACSimulatorRemoteNotificationsService.h
@@ -8,14 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
-
+/** ACSimulatorRemoteNotificationsService is a service to send mock remote notifications to SimulatorRemoteNotifications-enabled applications.
+ * 
+ * See UIApplication (SimulatorRemoteNotifications) for more details on how to enable mock remote notifications in an application.
+ */
 @interface ACSimulatorRemoteNotificationsService : NSObject
 
+/** @name Accessing the instance */
+
+/** Shared service
+ */
 + (instancetype)sharedService;
 
+/** @name Properties */
+
+/** Remote notifications port
+ * @discussion The UDP port of the host running the SimulatorRemoteNotifications-enabled application.
+ */
 @property (nonatomic, assign) NSInteger remoteNotificationsPort;
+
+/** Remote notificaitons host
+ * @discussion The IP address of the host running the SimulatorRemoteNotifications-enabled application.
+ */
 @property (nonatomic, copy) NSString *remoteNotificationsHost;
 
+
+/** @name Sending remote notifications */
+
+/** Send a mock remote notification
+ * @param payload Payload of the notification. Consult Apple's [Local and Push Notification Programming guide](https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Introduction.html#//apple_ref/doc/uid/TP40008194-CH1-SW1) for details about the notifications payload format.
+ */
 - (void)send:(NSDictionary*)payload;
 
 @end
