0c0925f6d9121a76479ae3bbdad3b38a5009d14e
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-02-22 21:10:49 -0800

    Added osx app

---
+++ iOS Simulator Notifications/ACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACAppDelegate.h
+//  iOS Simulator Notifications
+//
+//  Created by Arnaud Coomans on 22/02/14.
+//  Copyright (c) 2014 acoomans. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ACAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ iOS Simulator Notifications/ACAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ACAppDelegate.m
+//  iOS Simulator Notifications
+//
+//  Created by Arnaud Coomans on 22/02/14.
+//  Copyright (c) 2014 acoomans. All rights reserved.
+//
+
+#import "ACAppDelegate.h"
+
+@implementation ACAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
